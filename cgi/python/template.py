c89dea0f0c31926c87c3e0a82b8eb2c048d3e6c3
Jheng-Jhong Wu <goodwater.wu@gmail.com>
2016-11-02 15:50:38 +0800

    Add REMOTE_ADDR and REQUEST_METHOD to global variables

--- cgi/python/template.py
+++ cgi/python/template.py
@@ -11,17 +11,23 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-import cgi
+import cgi, os
 
-dict_request = {}
+REMOTE_ADDR = None
+REQUEST_METHOD = None
+dict_request = None
 
 def parseHttpRequest():
-    '''Separate HTTP request string into dictionary of {key: value}'''
-    form = cgi.FieldStorage()
-    keys = form.keys()
-
-    for key in keys:
-        dict_request.update({key: form.getvalue(key)})
+	'''Separate HTTP request string into dictionary of {key: value}'''
+	global REMOTE_ADDR
+	global REMOTE_HOST
+	global REQUEST_METHOD
+	global dict_request
+	REMOTE_ADDR = os.environ.get('REMOTE_ADDR')
+	REQUEST_METHOD = os.environ.get('REQUEST_METHOD')
+	form = cgi.FieldStorage()
+	keys = form.keys()
+	dict_request = {key: cgi.escape(form.getvalue(key)) for key in keys}
 
 print('Content-type: text/html')
 print()
@@ -30,18 +36,16 @@ print('<!DOCTYPE html>')
 print('<html>')
 print('''
 <head>
-    <meta charset="UTF-8">
+	<meta charset="UTF-8">
 	<title>MODIFY_HERE_1</title>
-    <!--[if lt IE 9]>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-    <link rel="stylesheet" type="text/css" href="MODIFY_HERE_2">
-    <script type="text/javascript" src="MODIFY_HERE_3"></script>
-    <noscript>Not support javascript</noscript>
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link rel="stylesheet" type="text/css" href="MODIFY_HERE_2">
+	<script type="text/javascript" src="MODIFY_HERE_3"></script>
+	<noscript>Not support javascript</noscript>
 </head>
 ''')
-print('''
-<body>
-</body>
-''')
+print('<body>')
+print('</body>')
 print('</html>')
