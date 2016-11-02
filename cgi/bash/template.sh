5c9784f1ed1aaef47e4f63d9773113b7c1030f03
Jheng-Jhong Wu <goodwater.wu@gmail.com>
2016-11-02 16:26:51 +0800

    Fix parseHttpRequest

--- cgi/bash/template.sh
+++ cgi/bash/template.sh
@@ -10,26 +10,22 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 LICENSE
 
-# Get HTTP request string
-function parseRequestString
-{
-	if [ -n "${CONTENT_LENGTH}" ]; then
-		read QUERY_STRING_POST -n ${CONTENT_LENGTH}
-		request=$(echo "${QUERY_STRING_POST}" | awk 'BEGIN { FS="[=&]" } { for(i=1;i<=NF;i++) print $i }')
-	else
-		request=$(echo "${QUERY_STRING}" | awk 'BEGIN { FS="[=&]" } { for(i=1;i<=NF;i++) print $i }')
-	fi
-}
-
 # Separate HTTP request string into keys and values
 function parseHttpRequest
 {
-	parseRequestString
+	if [ "${REQUEST_METHOD}" == "GET" ]; then
+		request=$(echo "${QUERY_STRING}" | awk 'BEGIN { FS="[=&]" } { for(i=1;i<=NF;i++) print $i }')
+	elif [ "${REQUEST_METHOD}" == "POST" ]; then
+		read QUERY_STRING_POST -n ${CONTENT_LENGTH}
+		request=$(echo "${QUERY_STRING_POST}" | awk 'BEGIN { FS="[=&]" } { for(i=1;i<=NF;i++) print $i }')
+	else
+		:
+	fi
 
 	declare -i i=0
 	declare -i key_index=0
 
-	for str in ${post_str}
+	for str in ${request}
 	do
 		if [ $((${i}%2)) -eq 0 ]; then
 			key[${key_index}]=${str}
